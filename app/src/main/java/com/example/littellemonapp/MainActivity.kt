32b471cdf742fcbae37edfeda5424c22facd2d2f
Waleed <waleed.alqahtani23@gmail.com>
2024-05-21 19:42:21 +0300

    Update files

--- app/src/main/java/com/example/littellemonapp/MainActivity.kt
+++ app/src/main/java/com/example/littellemonapp/MainActivity.kt
@@ -6,7 +6,6 @@ import Navigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavHostController
